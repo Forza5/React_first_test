77c1f3a6390230847b8e1e8e708215d85114ec91
Forza5 <ganp433@gmail.com>
2022-10-06 10:26:23 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState([
+    {
+      content: 'react를 배워봅시다'
+    }
+  ]);
+
+  const contentInput = useRef();
+
+  const addList = () => {
+    setValue([...value,{
+      content: contentInput.current.value
+    }])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='topInput'>
+        <input type="text" ref={contentInput} />
+        <button onClick={addList}>추가하기</button>
+      </div>
+      <div className='title'>
+        <h1>Todo List</h1>
+      </div>
+      <div className='lists'>
+        {
+          value.map((lists, i) => {
+            return(
+              <div className='list' key={i}>
+                <div className='text'>
+                  <p>{lists.content}</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
